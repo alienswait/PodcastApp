215406157d25ac15958321403d78b6a0e8dbad34
Mert can Ünek <mrtcnunk@gmail.com>
2024-12-02 23:19:37 +0300

    Initial Commit

---
+++ PodcastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastApp
+//
+//  Created by Mertcan Ünek on 2.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
